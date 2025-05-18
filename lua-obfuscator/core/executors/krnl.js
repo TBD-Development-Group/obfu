5b0ffa6696605888c83a59e0ec8544c61fd3a664
TBD-Development-Group <boumaazahamza30@gmail.com>
2025-05-18 11:23:03 +0200

    Update krnl.js

--- lua-obfuscator/core/executors/krnl.js
+++ lua-obfuscator/core/executors/krnl.js
@@ -1,1 +1,40 @@
-
+module.exports = {
+    transform: function(ast) {
+        // KRNL-specific transformations
+        if (ast.body && ast.body.length > 0) {
+            const check = {
+                type: 'IfStatement',
+                clauses: [{
+                    type: 'ElseifClause',
+                    condition: {
+                        type: 'BinaryExpression',
+                        operator: '~=',
+                        left: {
+                            type: 'CallExpression',
+                            base: { type: 'Identifier', name: 'getrenv' },
+                            arguments: []
+                        },
+                        right: {
+                            type: 'Identifier',
+                            name: '_G'
+                        }
+                    },
+                    body: {
+                        type: 'BlockStatement',
+                        body: [{
+                            type: 'ReturnStatement',
+                            arguments: []
+                        }]
+                    }
+                }]
+            };
+            
+            ast.body.unshift(check);
+        }
+    },
+    
+    postProcess: function(code) {
+        // Add KRNL-specific patterns
+        return `-- KRNL Protected\n${code}`;
+    }
+};
